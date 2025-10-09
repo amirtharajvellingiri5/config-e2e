1cfa8af89c036dd9f857fc28acabf2b79d20c808
amirtharajvellingiri5 <amirtharaj.v@outlook.com>
2025-10-09 23:11:56 +0530

    Fix count var and fail

--- main.tf
+++ main.tf
@@ -105,7 +105,7 @@ resource "aws_route" "default_route" {
 }
 
 resource "aws_route_table_association" "public_assoc" {
-  count          = length(aws_subnet.public)
+  count          = length(var.public_subnet_cidrs)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
